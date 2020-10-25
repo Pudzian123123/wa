2a897401e2cc4085a9b4c9f7c59893bdebb9dbb4
Glitch (acoustic-elderly-sundae) <none>
2020-10-25 22:29:22 +0000

    Before import of AllanGame/ModeratorBOT-Discord.js-V12- without specified path ./config.js:24146027/72 ./commands/ban.js:24146027/1085 ./package.json:24146027/2591

--- commands/help.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs'); 
-
-module.exports = (client, message, args, send) => {
- fs.readFile('./help.txt', (err, txt) => {
-  if (err) send.log(err)
-  send.channel('```asciidoc\n' + txt + '\n```');
- });
-
-}
--- config.js
+++
@@ -1,9 +0,0 @@
-let config = {
- TOKEN_BOT: "HERE-TOKEN",
- PREFIX_BOT: "z!",
- UNLIMITED_BANS: 3,
- UNLIMITED_KICKS: 10
-
-}
-
-module.exports = config;
