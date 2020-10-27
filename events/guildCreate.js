ac6f75532ffab99990f02add4c4dda135e2475a6
Glitch (acoustic-elderly-sundae) <none>
2020-10-27 07:09:10 +0000

    🎠💵 Checkpoint ./package.json:24146027/2101

--- events/guildCreate.js
+++
@@ -1,6 +0,0 @@
-let config = require('../config.json')
-
-module.exports = async (client,guild) => {
-    console.log(`Joined: ${guild.name}`)
-    client.connection.query(`INSERT INTO guild(id,prefix,lang) VALUES (${guild.id},'..','en')`)
-}
